8cef2700f0d286826a80f89d1e82bce6abb3efe6
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2024-04-23 23:24:35 +0530

    Server and Promises

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+
+
+console.log("a");
+console.log("b");
+new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("c");
+    resolve();
+  }, 3000);
+}).then(() => {
+  console.log("d");
+});
+console.log("e");
