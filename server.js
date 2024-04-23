8b23152e0877a92640e9d3a88272cdad09de09cc
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2024-04-23 23:42:31 +0530

    Request.url

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const http= require('http');
-const server= http.createServer((request,response)=>{
-    console.log("Hello Satyam");
-})
+const http = require("http");
+const server = http.createServer((request, response) => {
+  const url = request.url;
+    response.setHeader('Content-Type',"text/html")
+  if(url==='/home'){
+    response.write("Welcome to home page")
+  }else if(url=='/about'){
+    response.write("Welcome to about page")
+  }
+  else if(url==='/node'){
+    response.write("Welcome to node page")
+  }
+  response.end();
+});
 
-server.listen(4000)
-
+server.listen(4000);
